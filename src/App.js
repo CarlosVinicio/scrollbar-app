0ce317240fb8d62e9923878661ae3fd2c07c96e3
Carlos Vinicio Guamán González <cvguaman@atsistemas.com>
2022-04-06 19:32:22 +0200

    Resueltos estilos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import justifiedLayout from "justified-layout";
 import { getSections, getSegments } from "./api";
@@ -11,19 +11,25 @@ const config = {
 };
 
 function App() {
+
   let sectionStates = {};
 
   useEffect(() => {
     loadUi();
   }, []);
 
-  const loadUi = () => { 
-    getSections().then(sections => {
-      populateGrid(document.getElementById("grid"), sections);
-      window.scrollTo({ top: 10000 });
-    });
-  };
 
+  const loadUi = async() => {
+    const sections = await getSections();
+    if(sections){
+      populateGrid(document.getElementById("grid"), sections);
+  
+      // simulating directly jumping to random scroll position
+      window.scrollTo({ top: 0 });
+    }
+  }
+  
+  // populates grid node with all detached sections
   const populateGrid = (gridNode, sections) => {
     var sectionsHtml = "";
     var prevSectionEnd = config.sectionMargin;
@@ -32,100 +38,104 @@ function App() {
         ...section,
         lastUpdateTime: -1,
         height: estimateSectionHeight(section),
-        top: prevSectionEnd,
+        top: prevSectionEnd
       };
-
+  
       sectionsHtml += getDetachedSectionHtml(sectionStates[section.sectionId]);
-      prevSectionEnd +=
-        sectionStates[section.sectionId].height + config.sectionMargin;
+      prevSectionEnd += sectionStates[section.sectionId].height + config.sectionMargin;
     }
     gridNode.innerHTML = sectionsHtml;
-
-    gridNode
-      .querySelectorAll(".section")
-      .forEach(sectionObserver.observe.bind(sectionObserver));
-  };
-
-  const getDetachedSectionHtml = (section) => {
-    return `<div id="${section.sectionId}" class="section" style="width: ${
-      config.containerWidth
-    }px; height: ${estimateSectionHeight(section)}px;"></div>`;
-  };
-
+  
+    // observe each section for intersection with viewport
+    gridNode.querySelectorAll(".section").forEach(sectionObserver.observe.bind(sectionObserver));
+  }
+  
+  // generates detached section html, detached section has estimated height and no segments loaded
+  const getDetachedSectionHtml = (sectionState) => {
+    return `<div id="${sectionState.sectionId}" class="section" style="width: ${config.containerWidth}px; height: ${sectionState.height}px; top: ${sectionState.top}px; left: 0px";"></div>`;
+  }
+  
+  // estimates section height, taken from google photos blog
+  // Ideally we would use the average aspect ratio for the photoset, however assume
+  // a normal landscape aspect ratio of 3:2, then discount for the likelihood we
+  // will be scaling down and coalescing.
   const estimateSectionHeight = (section) => {
-    const unwrappedWidth =
-      (3 / 2) * section.totalImages * config.targetRowHeight * (7 / 10);
+    const unwrappedWidth = (3 / 2) * section.totalImages * config.targetRowHeight * (7 / 10);
     const rows = Math.ceil(unwrappedWidth / config.containerWidth);
     const height = rows * config.targetRowHeight;
-
+  
     return height;
-  };
-
-  function populateSection(sectionDiv, segments) {
+  }
+  
+  // populates section with actual segments html
+  const populateSection = (sectionDiv, segments) => {
     let sectionId = sectionDiv.id;
     let segmentsHtml = "";
     let prevSegmentEnd = config.segmentsMargin;
     for (const segment of segments) {
-      const segmentInfo = getSegmentHtmlAndHeight(segment, prevSegmentEnd);
+      const segmentInfo = getSegmentHtmlAndHeight(segment, prevSegmentEnd)
       segmentsHtml += segmentInfo.html;
       prevSegmentEnd += segmentInfo.height + config.segmentsMargin;
     }
-
+  
+    // add segments to section and calculate new height
     sectionDiv.innerHTML = segmentsHtml;
     const newSectionHeight = prevSegmentEnd;
-    const oldSectionHeight = sectionStates[sectionId].height;
-
+    const oldSectionHeight = sectionStates[sectionId].height
+  
+    // adjust all next section's top if height of this section was modified
     const heightDelta = newSectionHeight - oldSectionHeight;
-    if (heightDelta == 0) {
-      return;
-    }
-
+    if (heightDelta == 0) { return }
+  
     sectionStates[sectionId].height = newSectionHeight;
     sectionDiv.style.height = `${newSectionHeight}px`;
-
-    Object.keys(sectionStates).forEach((sectionToAdjustId) => {
-      if (sectionToAdjustId >= sectionId) {
-        return;
-      }
-
+  
+    Object.keys(sectionStates).forEach(sectionToAdjustId => {
+      if (sectionToAdjustId >= sectionId) { return }
+  
       sectionStates[sectionToAdjustId].top += heightDelta;
       const sectionToAdjustDiv = document.getElementById(sectionToAdjustId);
       sectionToAdjustDiv.style.top = `${sectionStates[sectionToAdjustId].top}px`;
     });
-
+  
     // adjust scroll if user is scrolling upwords and we loaded some section above current scroll position
     if (window.scrollY > sectionStates[sectionId].top) {
       window.scrollBy(0, heightDelta);
     }
   }
-
+  
+  // generates Segment html and height
   const getSegmentHtmlAndHeight = (segment, top) => {
-    const sizes = segment.images.map((image) => image.metadata);
+    const sizes = segment.images.map(image => image.metadata);
     var geometry = justifiedLayout(sizes, config);
-
+  
+    // gets tiles for each box given by justified layout lib
     var tiles = geometry.boxes.map(getTileHtml).join("\n");
-
+  
     return {
       html: `<div id="${segment.segmentId}" class="segment" style="width: ${config.containerWidth}px; height: ${geometry.containerHeight}px; top: ${top}px; left: 0px;">${tiles}</div>`,
-      height: geometry.containerHeight,
+      height: geometry.containerHeight
     };
-  };
-
+  }
+  
+  // generates Tile html
   const getTileHtml = (box) => {
     return `<div class="tile" style="width: ${box.width}px; height: ${box.height}px; left: ${box.left}px; top: ${box.top}px;"></div>`;
-  };
-
+  }
+  
+  // detaches section by removing childs of section div and keeping same height
   const detachSection = (sectionDiv) => {
     sectionDiv.innerHTML = "";
-  };
-
+  }
+  
+  // handle when there is change for section intersecting viewport
   const handleSectionIntersection = (entries, observer) => {
     entries.forEach((entry) => {
       const sectionDiv = entry.target;
       sectionStates[sectionDiv.id].lastUpdateTime = entry.time;
-
+  
       if (entry.isIntersecting) {
-        getSegments(sectionDiv.id).then((segments) => {
+        getSegments(sectionDiv.id).then(segments => {
           window.requestAnimationFrame(() => {
             if (sectionStates[sectionDiv.id].lastUpdateTime === entry.time) {
               populateSection(sectionDiv, segments);
@@ -135,19 +145,22 @@ function App() {
       } else {
         window.requestAnimationFrame(() => {
           if (sectionStates[sectionDiv.id].lastUpdateTime === entry.time) {
-            detachSection(sectionDiv, entry.time);
+            detachSection(sectionDiv, entry.time)
           }
         });
       }
     });
-  };
+  }
 
   const sectionObserver = new IntersectionObserver(handleSectionIntersection, {
-    rootMargin: "200px 0px",
+    rootMargin: "200px 0px"
   });
+  
 
   return (
-    <div id="grid" className="scrubbable-grid"></div>
+    <div >
+      <div id="grid" className="scrubbable-grid"></div>
+    </div>
   );
 }
 
--- src/api.js
+++ src/api.js
@@ -1,24 +1,34 @@
-import store from "./store.json";
+function delay(ms) {
+  return function (x) {
+    return new Promise((resolve) => setTimeout(() => resolve(x), ms));
+  };
+}
 
-let delayTimer;
-
+// get all sections in users photo library - e.g. one section per month
 export const getSections = () => {
-  return new Promise((resolve) => {
-    clearTimeout(delayTimer);
-    delayTimer = setTimeout(function () {
-      resolve(store);
-    }, 1000);
-  });
-};
+  return fetch("store.json" )
+    .then((res) => {
+      const response = res.json();
+      return response;
+    })
+    // .then(delay(50 + Math.random() * 500))
+    .then((store) => {
+      const sections = store.sections.map((section) => {
+        return {
+          sectionId: section.sectionId,
+          totalImages: section.totalImages,
+        };
+      });
+      return sections
+    });
+}
 
-export const getSegments = (sectionId) => {
-  return new Promise((resolve) => {
-    const response = store.find(
-      (section) => section.sectionId == sectionId
-    ).segments;
-    clearTimeout(delayTimer);
-    delayTimer = setTimeout(function () {
-      resolve(response);
-    }, 1000);
-  });
-};
+// get all segments inside one section - e.g. one segment per day
+export function getSegments(sectionId) {
+  return fetch("./store.json")
+    .then((res) => res.json())
+    .then(delay(50 + Math.random() * 500))
+    .then((store) => {
+      return store.sections.find((section) => section.sectionId == sectionId).segments;
+    });
+}
